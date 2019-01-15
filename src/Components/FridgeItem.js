13d9cd734a2c78dbba66701e2c7627054233c486
gthomas2018 <garrettthomas2018@u.northwestern.edu>
2019-01-15 12:34:55 -0600

    Another fun commit

--- src/Components/FridgeItem.js
+++ src/Components/FridgeItem.js
@@ -6,10 +6,21 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 //item name, days until expiration
 
 export default class FridgeItem extends React.Component{
+    prettyDate(date) {
+        if (date === 1)
+        {
+            return String(date) + " day";
+        }
+        else
+        {
+            return String(date) + " days";
+        }
+    }
+
 	render() {
         return(
             <ListItem>
-                <ListItemText primary={this.props.item} secondary={this.props.date} />
+                <ListItemText primary={this.props.item} secondary={this.prettyDate(this.props.date)} />
                 <ListItemSecondaryAction>
 		              <i className="material-icons">
 			              delete
