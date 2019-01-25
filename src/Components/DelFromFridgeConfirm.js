47543126f2a74a81853d90a8b3550d110231ed93
Jodie Wei <jodiewei2019@u.northwestern.edu>
2019-01-25 01:50:35 -0600

    add quantities to fridge, allow adding and removing quantities

---
+++ src/Components/DelFromFridgeConfirm.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default class DelConfirm extends React.Component {
+	del_and_close = (item, quantity) => {
+		this.props.delItem(item, quantity);
+		this.props.closePopup();
+	}
+
+	render() {
+    return (
+      <div className="popup">
+        <div className="popup_inner"> 
+          <div className="title">Delete how many {this.props.item}?</div>
+          <br />
+          <div>Quantity: <input type="number" id="quantity"/></div>
+          <br />
+          <button className="popup_button left" onClick={() => this.del_and_close(this.props.item, document.getElementById('quantity').value)}>
+            Ok
+            </button>
+	        <button className="popup_button right" onClick={this.props.closePopup}>Cancel</button>
+        </div>
+      </div>
+		)
+	}
+}
---
+++ src/Components/DelFromShoppingConfirm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default class DelConfirm extends React.Component {
+	del_and_close = (item) => {
+		this.props.delItem(item);
+		this.props.closePopup();
+	}
+
+	render() {
+    return (
+      <div className="popup">
+        <div className="popup_inner"> 
+          <div className="title">Delete {this.props.item}?</div>
+          <br />
+          <button className="popup_button left" onClick={() => this.del_and_close(this.props.item)}>
+            Ok
+            </button>
+	        <button className="popup_button right" onClick={this.props.closePopup}>Cancel</button>
+        </div>
+      </div>
+		)
+	}
+}
