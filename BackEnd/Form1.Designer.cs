8d8950285a5e680f3104599009413b9dec16e318
Molly Lansford <mollylansford2019@u.northwestern.edu>
2019-02-10 11:43:50 -0600

    Back end service to update database at midnight and send emails. Removed email login information

---
+++ BackEnd/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SpoilerAlertBackEnd
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.activityLabel = new System.Windows.Forms.Label();
+      this.lastCheckedTimeLabel = new System.Windows.Forms.Label();
+      this.lastCheckedDatabaseLabel = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // activityLabel
+      // 
+      this.activityLabel.AutoSize = true;
+      this.activityLabel.Location = new System.Drawing.Point(82, 38);
+      this.activityLabel.Name = "activityLabel";
+      this.activityLabel.Size = new System.Drawing.Size(38, 17);
+      this.activityLabel.TabIndex = 0;
+      this.activityLabel.Text = "Start";
+      // 
+      // lastCheckedTimeLabel
+      // 
+      this.lastCheckedTimeLabel.AutoSize = true;
+      this.lastCheckedTimeLabel.Location = new System.Drawing.Point(82, 94);
+      this.lastCheckedTimeLabel.Name = "lastCheckedTimeLabel";
+      this.lastCheckedTimeLabel.Size = new System.Drawing.Size(85, 17);
+      this.lastCheckedTimeLabel.TabIndex = 1;
+      this.lastCheckedTimeLabel.Text = "Last ran tick";
+      // 
+      // lastCheckedDatabaseLabel
+      // 
+      this.lastCheckedDatabaseLabel.AutoSize = true;
+      this.lastCheckedDatabaseLabel.Location = new System.Drawing.Point(82, 155);
+      this.lastCheckedDatabaseLabel.Name = "lastCheckedDatabaseLabel";
+      this.lastCheckedDatabaseLabel.Size = new System.Drawing.Size(157, 17);
+      this.lastCheckedDatabaseLabel.TabIndex = 2;
+      this.lastCheckedDatabaseLabel.Text = "Last checked Database";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(562, 253);
+      this.Controls.Add(this.lastCheckedDatabaseLabel);
+      this.Controls.Add(this.lastCheckedTimeLabel);
+      this.Controls.Add(this.activityLabel);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label activityLabel;
+    private System.Windows.Forms.Label lastCheckedTimeLabel;
+    private System.Windows.Forms.Label lastCheckedDatabaseLabel;
+  }
+}
+
---
+++ BackEnd/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Mail;
+
+namespace SpoilerAlertBackEnd
+{
+  public partial class Form1 : Form
+  {
+    Timer activityTimer;
+    DateTime lastChecked = DateTime.MinValue;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      activityLabel.Text = "Started";
+      activityTimer = new Timer();
+      activityTimer.Interval = 5000;
+      activityTimer.Tick += ActivityTimer_Tick;
+      activityTimer.Start();
+    }
+
+    private void ActivityTimer_Tick(object sender, EventArgs e)
+    {
+      activityTimer.Stop();
+      try
+      {
+        lastCheckedTimeLabel.Text = "Last Ran Tick: " + DateTime.Now;
+        if (lastChecked.Date != DateTime.Today)
+        {
+          lastCheckedDatabaseLabel.Text = "Last Checked Database: " + DateTime.Now;
+          lastChecked = DateTime.Today;
+          FireSharp.Config.FirebaseConfig c = new FireSharp.Config.FirebaseConfig();
+          c.AuthSecret = "NI7XHYpZL97crh1Hfh7biXC3C4FQvJIRzi8ZetcX";
+          c.BasePath = "https://spoileralert-394.firebaseio.com";
+          FireSharp.FirebaseClient f = new FireSharp.FirebaseClient(c);
+          FireSharp.Response.FirebaseResponse r = f.Get("/");
+
+          dynamic database = JsonConvert.DeserializeObject(r.Body);
+
+          foreach (var user in database)
+          {
+            List<String> expiredItems = new List<string>();
+            var id = user.Name;
+            var u = user.First;
+            var settings = u["settings"];
+            int threshold = settings["threshold"]; 
+            String emailAddress = settings["email"]; 
+            bool wantNotifications = settings["notifications"];
+            var l = u["fridge"];
+            if (l == null)
+            {
+              continue;
+            }
+            foreach (var item in l)
+            {
+              var name = item.Name;
+              var arr = item.Value;
+              int quantity = (int)arr.First.Value;
+              int daysTil = (int)arr.Last.Value;
+              int updateData = daysTil - 1;
+              int[] newVal = new int[2] { quantity, updateData };
+
+              if (updateData <= threshold)
+              {
+                expiredItems.Add(name);
+              }
+
+              // ---- Update the database ----- //
+              String path = String.Format("/{0}/fridge/{1}", id, name);
+              
+              f.Set(path, newVal);
+            }
+
+            // ---- Email User with any expired items
+            if (wantNotifications && (expiredItems.Count > 0))
+            {
+              String body = "Hello there!\nThe following items from your fridge expire in ";
+              body = body + threshold;
+              body = body + " days: \n";
+              foreach (var s in expiredItems){
+                body = body + s;
+                body = body + "\n";
+              }
+
+              body = body + "\nStay Fresh!\nSpoilerAlert";
+              String subject = "A Message From Your Fridge";
+
+              MailMessage mail = new MailMessage("SpoilerAlert <youremail@gmail.com>", emailAddress);
+              SmtpClient client = new SmtpClient("smtp.gmail.com");
+              client.Port = 587;
+              client.EnableSsl = true;
+              client.DeliveryMethod = SmtpDeliveryMethod.Network;
+              client.UseDefaultCredentials = false;
+              client.Credentials = new System.Net.NetworkCredential("youremail", "yourpassword");
+              
+              mail.Subject = subject;
+              mail.Body = body;
+              client.Send(mail);
+            }
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.ToString());
+      }
+      finally
+      {
+        activityTimer.Start();
+      }
+    }
+  }
+}
